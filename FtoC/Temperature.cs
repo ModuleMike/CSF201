18c05fbbcc7ea2edb0fafeb842c2d1205e09a129
ModuleMike <146703098+ModuleMike@users.noreply.github.com>
2024-01-03 23:46:00 -0800

    Add files via upload

---
+++ FtoC/Temperature.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempConverter
+{
+    public class Temperature
+    {
+        private double _kelvinTemp;
+        public double Kelvin
+        {
+            get { return _kelvinTemp; }
+            set
+            {
+                if (value < 0)
+                {
+                    _kelvinTemp = 0;
+                }
+                else
+                {
+                    _kelvinTemp = value;
+                }
+            }
+        }
+
+        public double Celsius
+        {
+            get
+            {
+                return Kelvin - 273.15;
+            }
+        }
+
+        public double Fahrenheit
+        {
+            get
+            {
+                return Celsius * 9 / 5 + 32;
+            }
+        }
+
+    }
+}
